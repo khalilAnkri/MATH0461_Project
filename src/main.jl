d1599c3737664562bf4e800c6368e33b63033c43
AntoinePaulis <paulisantoine@gmail.com>
2025-10-18 11:56:43 +0200

    Add time and plot

--- src/main.jl
+++ src/main.jl
@@ -1,7 +1,11 @@
 include("data.jl")    # Provides consumption_1yr, irradiance_1yr
 include("model.jl")   # Uses consumption_1yr, irradiance_1yr
+include("analysis.jl")
 
-optimize!(model)
+@time optimize!(model)
 
 println("Optimal PV capacity (Wp): ", value(capacityPanel))
 println("Optimal battery capacity (Wh): ", value(capacityBattery))
+
+plot_week(Array(value.(PPV)),Array(value.(PBPlus)),Array(value.(PBMinus)),Array(value.(PGPlus)),
+            Array(value.(PGMinus)),Array(value.(E)),consumption)
