b8cd1f44c847bfe5625d3d3f6e3a73836b9fdd5c
AntoinePaulis <paulisantoine@gmail.com>
2025-10-26 14:57:56 +0100

    Add the use of Q7/8 model

--- src/main.jl
+++ src/main.jl
@@ -1,12 +1,16 @@
 include("data.jl")      # Provides consumption_1yr, irradiance_1yr
 include("model.jl")     # Uses consumption_1yr, irradiance_1yr
 include("analysis.jl")
-#include("modelQ4.jl")   # Provides the model related to the question 4
+#include("modelQ4.jl")
+#include("modelQ7.jl")
+#include("modelQ8.jl")
 
 @time optimize!(model)
 
 println("Optimal PV capacity (Wp): ", value(capacityPanel))
 println("Optimal battery capacity (Wh): ", value(capacityBattery))
+#println("Optimal wind capacity (Wp) :", value(capacityWind))
+#println("Optimal value of S :", value(S))
 
-plot_week(Array(value.(PPV)),Array(value.(PBPlus)),Array(value.(PBMinus)),Array(value.(PGPlus)),
-            Array(value.(PGMinus)),Array(value.(E)),consumption)
+plot_week(Array(value(PPV)),Array(value(PBPlus)),Array(value(PBMinus)),Array(value(PGPlus)),
+            Array(value(PGMinus)),Array(value(E)),consumption)
