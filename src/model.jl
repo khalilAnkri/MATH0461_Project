412e1169961637cd3c8f53eb6cd6ecfe0d4b6ddb
AntoinePaulis <paulisantoine@gmail.com>
2025-11-02 12:26:30 +0100

    Update objective function

--- src/model.jl
+++ src/model.jl
@@ -7,6 +7,7 @@ include("data.jl")  # provides: consumption, irradiance
 T = length(consumption)
 time = 1:T
 deltat = 1.0  # hours
+years = T*deltat/(24*365)  
 
 # -----------------------------
 # System parameters
@@ -26,7 +27,7 @@ AP = 20
 # -----------------------------
 
 model = Model(Gurobi.Optimizer)
-set_optimizer_attribute(model, "Method", 3)  # 1 for the Simplex algo and 3 for barrier method
+set_optimizer_attribute(model, "Method", 1)  # 1 for the Simplex algo and 3 for barrier method
 
 # -----------------------------
 # Decision variables
@@ -65,19 +66,9 @@ set_optimizer_attribute(model, "Method", 3)  # 1 for the Simplex algo and 3 for 
 # -----------------------------
 # Objective function
 # -----------------------------
-# No amortization needed for 24h, but PV/battery fractions scaled to kW/kWh
+  
 @objective(model, Min,
-    (costPV/AP)*(capacityPanel/1000) +
-    (costBattery/AP)*(capacityBattery/1000) +
-    sum((costGPlus*PGPlus[t]*deltat/1000 - costGMinus*PGMinus[t]*deltat/1000) for t in time)
+    costPV*(years/AP)*capacityPanel +
+    costBattery*(years/AP)*capacityBattery +
+    sum((costGPlus*PGPlus[t]*deltat- costGMinus*PGMinus[t]*deltat) for t in time)
 )
-
-# -----------------------------
-# Comment section
-# -----------------------------
-# I have added the fractions "/1000" in the objective function to ensure that the units are consistent,
-#  converting W to kW and Wh to kWh where necessary for cost calculations.
-# Comment section
-# -----------------------------
-# I have added the fractions "/1000" in the objective function to ensure that the units are consistent,
-#  converting W to kW and Wh to kWh where necessary for cost calculations.
